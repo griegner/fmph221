20decd419147aecc79f497d339d62913ee6dcfd5
Gabriel Riegner <gabriel.riegner@gmail.com>
2022-10-21 15:27:26 -0700

    add template files

---
+++ scripts/01-project-proposal.r
@@ -0,0 +1,11 @@
+# load ../data/neuromaps-voxel.csv into data frame
+## column 1 is the response, and the rest are predictors
+
+
+# pairs plots
+
+
+# data frame description: sampling size, summary statistics, apparent trends etc
+
+
+# initial analysis: multiple regression, interpretation, basic diagnostics
